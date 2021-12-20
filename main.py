1a61e0368218d3f663f04984c8d1d9f7a648bc15
Ming-der Wang <>
2021-12-20 16:01:46 +0000

    final

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class RequestHandler(BaseHTTPRequestHandler):
             {
                 "id": str(uuid.uuid4()),
                 "選號日期": time.strftime("%m/%d/%Y", time.localtime()),
-                "可能中獎號碼": random.sample(range(10), 3),
+                "可能中獎號碼": random.sample(range(10), 4),
                 "猜中機率": 0
             },
             ensure_ascii=False)
